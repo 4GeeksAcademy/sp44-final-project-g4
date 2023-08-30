277ddab9d19c333e42bf9e4ce2af8a8533dd467c
Hector Padron <helpad93@gmail.com>
2023-08-30 19:22:33 +0000

    feat: finished end-points and groomers models

--- src/api/models/groomer_favorite_model.py
+++ src/api/models/groomer_favorite_model.py
@@ -7,28 +7,22 @@ import uuid
 db = SQLAlchemy()
 
 
-class GroomerReviewsModel(db.Model):
-    __tablename__ = "groomerreviews"
+class GroomerFavoritesModel(db.Model):
+    __tablename__ = "groomerfavorites"
     id = db.Column(db.BigInteger, default=lambda: uuid.uuid4().int >> (128 - 32), primary_key=True)
-    title = db.Column(db.String(80), unique=False, nullable=True)
-    body = db.Column(db.String(200), unique=False, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    rate = db.Column(db.String(80), unique=False, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship("User", back_populates="groomerreviews")
+    user = db.relationship("User", back_populates="groomerfavorites")
     groomer_id = db.Column(db.Integer, db.ForeignKey('groomer.id'))
-    groomer = db.relationship("GroomerModel", back_populates="groomerreviews")
+    groomer = db.relationship("GroomerModel", back_populates="groomerfavorites")
     
     def __repr__(self):
-        return f'<groomerreviews {self.id}>'
+        return f'<groomerfavorites {self.id}>'
 
     def serialize(self):
         return {
             "id": self.id,
-            "user_id": self.user_id,
-            "groomer_id": self.groomer_id,
-            "title": self.title,
-            "body": self.body,
             "created_at": self.created_at,
-            "rate": self.rate
+            "user_id": self.user_id,
+            "groomer_id": self.groomer_id    
         }
