97380cf3265ea398c63ca6ab1e4a045c11917067
Romu9319 <129613538+Romu9319@users.noreply.github.com>
2023-08-30 18:50:36 +0000

    Walker models finish

--- src/api/models/user_model.py
+++ src/api/models/user_model.py
@@ -1,18 +1,37 @@
-from db import db
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import uuid
+
+db = SQLAlchemy()
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    __tablename__ = "user"
+    id = db.Column(db.BigInteger, default=lambda: uuid.uuid4().int >> (128 - 32), primary_key=True)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    name = db.Column(db.String(100), unique=False, nullable=False)
+    last_name = db.Column(db.String(100), unique=False, nullable=True)
+    phone_number = db.Column(db.String(150), unique=True)
+    avatar = db.Column(db.String(2000), unique=False)
+    password = db.Column(db.String(100), unique=False, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    is_admin = db.Column(db.Boolean, default=False)
+    is_active = db.Column(db.Boolean(), default=False)
+   
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User {self.id} {self.email}>'
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "last_name": self.last_name,
+            "phone_number": self.phone_number,
+            "avatar": self.avatar,
+            "is_active": self.is_active,
+            "is_admin": self.is_admin,
+            "created_at": self.created_at
         }
+
