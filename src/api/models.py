8909bd36f1d99284be96c1fa1ed79d134ba1fde1
El Hadj Mbengue Arroyo <109061270+arroyostack@users.noreply.github.com>
2023-09-02 13:24:45 +0000

    Fixed: models and routes duplicates

--- src/api/models.py
+++ src/api/models.py
@@ -133,34 +133,7 @@ class VetFavoriteModel(db.Model):
             "vet_id": self.vet_id,
         }
 
-class PostModel(db.Model):
-    __tablename__ = "post"
-    id = db.Column(db.BigInteger, default=lambda: uuid.uuid4().int >> (
-        128 - 32), primary_key=True)
-    title = db.Column(db.String(300), unique=False, nullable=False)
-    body = db.Column(db.String(3000), unique=False, nullable=False)
-    image = db.Column(db.String(3000), unique=False, nullable=False)
-    category_enum = db.Enum('Salud', 'Bienestar',
-                            'Belleza', name='category_enum')
-    author = db.Column(db.String(200), unique=False, nullable=False)
-    category = db.Column(category_enum, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return f'<Post: {self.id} {self.email}>'
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            "body": self.body,
-            "date": self.date,
-            "image": self.image,
-            "category": self.category,
-            "author": self.author,
-            "category": self.category
-        }
-      
 class Address(db.Model):
     __table_name__="AddressModel"
     id = db.Column(db.BigInteger, default=lambda: uuid.uuid4().int >> (
@@ -245,6 +218,9 @@ class GroomerReviewsModel(db.Model):
     
     def __repr__(self):
         return f'<groomerreviews {self.id}>'
+    
+    def serialize(self):
+        return {
             "name": self.name,
             "last_name": self.last_name,
             "phone_number": self.phone_number,
@@ -256,41 +232,7 @@ class GroomerReviewsModel(db.Model):
             "price_high": self.price_high,
         }
     
-class VetReviewModel(db.Model):
-    __tablename__ = "vet_review"
-    id = db.Column(db.BigInteger, default=lambda: uuid.uuid4().int >> (
-        128 - 32), primary_key=True)
-    title = db.Column(db.String(300), unique=False, nullable=False)
-    body = db.Column(db.String(3000), unique=False, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    rate_enum = db.Enum('1', '2', '3', '4', '5', name='rate_enum')
-    rate = db.Column(rate_enum, nullable=False)
-    # Relations
-    user_id = db.Column(
-        db.BigInteger, db.ForeignKey("user.id"), unique=False, nullable=False
-    )
-    user = db.relationship("User")
-    vet_id = db.Column(
-        db.BigInteger, db.ForeignKey("vet.id"), unique=False, nullable=False
-    )
-    # One to many
-    vet = db.relationship("VetModel")
 
-    def __repr__(self):
-        return f'<Review: {self.id} >'
-
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "groomer_id": self.groomer_id,
-            "title": self.title,
-            "body": self.body,
-            "created_at": self.created_at,
-            "rate": self.rate
-        }
-    
 class GroomerFavoritesModel(db.Model):
     __tablename__ = "groomerfavorites"
     id = db.Column(db.BigInteger, default=lambda: uuid.uuid4().int >> (128 - 32), primary_key=True)
@@ -302,6 +244,9 @@ class GroomerFavoritesModel(db.Model):
     
     def __repr__(self):
         return f'<groomerfavorites {self.id}>'
+    
+    def serialize(self):
+        return {
             "title": self.title,
             "body": self.body,
             "date": self.created_at,
@@ -309,32 +254,7 @@ class GroomerFavoritesModel(db.Model):
             "user_id": self.user_id,
             "vet_id": self.vet_id,
         }
-    
-class VetFavoriteModel(db.Model):
-    __tablename__ = "vet_favorite"
-    id = db.Column(db.BigInteger, default=lambda: uuid.uuid4().int >> (
-        128 - 32), primary_key=True)
-    user_id = db.Column(db.BigInteger, db.ForeignKey(
-        'user.id'), unique=False, nullable=False)
-    vet_id = db.Column(
-        db.BigInteger, db.ForeignKey("vet.id"), unique=False, nullable=False
-    )
-    # One to many
-    vet = db.relationship("VetModel")
-    user = db.relationship("User")
 
-    def __repr__(self):
-        return f'<Favorite Vet {self.id}>'
-
-    def serialize(self):
-        return {
-            "id": self.id,
-
-            "created_at": self.created_at,
-            "user_id": self.user_id,
-            "groomer_id": self.groomer_id    
-        }
-    
 class WalkerModel(db.Model):
     __tablename__ = "walker"
     id = db.Column(db.BigInteger, default=lambda: uuid.uuid4().int >> (
@@ -352,6 +272,9 @@ class WalkerModel(db.Model):
 
     def __repr__(self):
         return f'<Walker {self.name} {self.surname}'
+    
+    def serialize(self):
+        return {
             "user_id": self.user_id,
             "vet_id": self.vet_id,
         }
@@ -431,6 +354,9 @@ class FavoriteWalkers(db.Model):
                 "User ID": self.user_id,
                 "Walker ID": self.walker_id
                 }
+    
+    def serialize(self):
+        return {
             "title": self.title,
             "body": self.body,
             "date": self.date,
@@ -441,29 +367,5 @@ class FavoriteWalkers(db.Model):
         }
     
     
-class Address(db.Model):
-    __table_name__="AddressModel"
-    id = db.Column(db.BigInteger, default=lambda: uuid.uuid4().int >> (
-        128 - 32), primary_key=True)
-    street = db.Column(db.String(400), unique=False, nullable=False)
-    number = db.Column(db.Integer, unique=False, nullable=False)
-    zip_code = db.Column(db.Integer, unique=False, nullable=False)
-    user_id = db.Column(
-        db.BigInteger, db.ForeignKey("user.id"), unique=False, nullable=False
-    )
-    user = db.relationship("User")
-
-    def __repr__(self):
-        return f'<Adress: {self.id} {self.user_id}>'
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "street": self.street,
-            "body": self.body,
-            "number": self.number,
-            "zip_code": self.zip_code,
-            "user_id": self.user_id,
-        }
 
 
