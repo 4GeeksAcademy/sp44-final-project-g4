196ef298381c540a35914a1cfd08c472c9bb3a1a
Yúliya Bundur <ybundur@gmail.com>
2023-09-09 21:10:32 +0000

    working with jumbotron

--- src/front/js/component/jumbotron/Jumbotron.jsx
+++ src/front/js/component/jumbotron/Jumbotron.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 export const Jumbotron = () => {
     return (
-        <div class="jumbotron">
-        <h1>Mi Título Destacado</h1>
-        <p>Este es un mensaje importante que deseamos destacar.</p>
-        <a class="btn btn-primary btn-lg" href="#" role="button">Encontrar especialista</a>
-</div>
+        <div class="jumbotron jumbotron-fluid">
+        <h1 class="card-title-jumbotron"><b>All</b> the services <b>for your pet</b> in one place</h1>
+        <h4 class="card-text-jumbotron">Este es un mensaje importante que deseamos destacar.</h4>
+        <a class="btn btn-outline-warning btn-lg" href="https://images.pexels.com/photos/160472/doll-boy-cheeky-toys-160472.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" role="button">Encontrar especialista</a>
+        </div>
     )
 }
