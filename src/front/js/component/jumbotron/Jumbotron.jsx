81220fdc266520542eec245b0a8eb188144131c1
Yúliya Bundur <ybundur@gmail.com>
2023-09-09 22:20:08 +0000

    jumbotron changes and css

--- src/front/js/component/jumbotron/Jumbotron.jsx
+++ src/front/js/component/jumbotron/Jumbotron.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 export const Jumbotron = () => {
     return (
-        <div class="jumbotron jumbotron-fluid">
+        <div class="jumbotron jumbotron-fluid bg-light-blue px-3 py-5">
         <h1 class="card-title-jumbotron"><b>All</b> the services <b>for your pet</b> in one place</h1>
         <h4 class="card-text-jumbotron">Este es un mensaje importante que deseamos destacar.</h4>
-        <a class="btn btn-outline-warning btn-lg" href="https://images.pexels.com/photos/160472/doll-boy-cheeky-toys-160472.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" role="button">Encontrar especialista</a>
+        <a class="btn btn-info btn-lg" href="https://images.unsplash.com/photo-1535930749574-1399327ce78f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1936&q=80" role="button"><h5>Encontrar especialista</h5></a>
         </div>
     )
 }
