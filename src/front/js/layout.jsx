d43146413818eec63c947089367ccd0ec7a2ebc1
Hector Padron <helpad93@gmail.com>
2023-09-10 16:17:06 +0000

    correcion

--- src/front/js/layout.jsx
+++ src/front/js/layout.jsx
@@ -4,16 +4,10 @@ import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home.jsx";
-import { VetView } from "./pages/vetView.jsx";
-import { GroomerView } from "./pages/groomerView.jsx";
-import { WalkerView } from "./pages/walkerView.jsx";
-import { AdoptionView } from "./pages/adoptionView.jsx";
 import injectContext from "./store/appContext";
+import { Login } from "./pages/Login.jsx";
+import { Navbar } from "./component/Navbar.jsx";
 
-import { CardVet } from "./component/cards/CardVet.jsx";
-import { CardGroomer } from "./component/cards/CardGroomer.jsx";
-import { CardWalker } from "./component/cards/CardWalker.jsx";
-import { CardAdoption } from "./component/cards/CardAdoption.jsx";
 
 //create your first component
 const Layout = () => {
@@ -21,26 +15,21 @@ const Layout = () => {
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
+    if ( !process.env.BACKEND_URL || process.env.BACKEND_URL == "" ) return <BackendURL />;
 
     return (
         <div>
-            <BrowserRouter basename={basename}>
+            <BrowserRouter basename={ basename }>
                 <ScrollToTop>
-                    {/* <Navbar /> */}
+                    <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<VetView />} path="/vet" />
-                        <Route element={<GroomerView />} path="/groomer" />
-                        <Route element={<WalkerView />} path="/walker" />
-                        <Route element={<AdoptionView />} path="/adoption" />
-                        <Route element={<h1>Not found!</h1>} />
+                       
+                     
                     </Routes>
-                    <p>Footer</p>
                 </ScrollToTop>
             </BrowserRouter>
         </div>
     );
 };
 
-export default injectContext(Layout);
+export default injectContext( Layout );
--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -2,34 +2,31 @@ import React from "react";
 import { useContext } from "react";
 import { Context } from "../store/appContext";
 import { Navbar } from "../component/navbar/Navbar.jsx"
-import { LowerView } from "../component/lower_view/LowerView.jsx";
+import { Jumbotron } from "../component/jumbotron/Jumbotron.jsx";
+
 
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+	const { store, actions } = useContext( Context );
 
 	return (
-		<>    
-		{/* modificar el home */}
-			<div className="container">
-				<div className="navbar">
-				<h1>
-					Spain 44 final project
-					<Navbar />
-				</h1>
-				</div>
-				<div className="jumbotron">
-					{/*Jumbotron */}
-					<img src="https://cdn.pixabay.com/photo/2017/04/11/15/55/animals-2222007_1280.jpg" alt="imagen jumbotron" />
-				</div>
-				<div className="lowerView">
-					<LowerView />
-				</div>
-				<div className="footer">
-					{/*Footer*/}
-					<p>Footer</p>
-				</div>
-			</div>
+		<>
+			<h1>
+				Spain 44 final project
+			</h1>
+
+			{/*Navbar */ }
+
+			{/*Jumbotron */}
+			<Jumbotron />
+
+			{/*END of Navbar */ }
+
+			{/*Jumbotron */ }
+
+			{/*Select Professional*/ }
+
+			{/*Footer*/ }
 		</>
 	);
 };
