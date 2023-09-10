179096081327f0a31c94f0f80e3a55773d916121
Hector Padron <helpad93@gmail.com>
2023-09-10 15:34:52 +0000

    feat: Views

--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -27,7 +27,7 @@ const injectContext = PassedComponent => {
 			 * store, instead use actions, like this:
 			 **/
 			state.actions.getMessage(); // <---- calling this function from the flux.js actions
-			state.actions.getPeople(); // asi se llama la funcion que viene del flux
+			// state.actions.getPeople(); // asi se llama la funcion que viene del flux
 		}, []);
 
 		// The initial value for the context is not null anymore, but the current state of this component,
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -10,7 +10,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 				//modificar para el fetch de la base de datos
 			getPeople: async () => {
 				if (localStorage.getItem("peopleLocal") === null) {
-					const host = "https://www.swapi.tech/api";
+					const host = "https://serpapi.com/search.json?engine=google&q=Coffee";
 					const url = host + '/people/';
 					const request = {
 						method: "GET",
@@ -47,32 +47,32 @@ const getState = ({ getStore, getActions, setStore }) => {
 				getActions().changeColor(0, "green");
 			},
 
-			// getMessage: async () => {
-			// 	try{
-			// 		// fetching data from the backend
-			// 		const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-			// 		const data = await resp.json()
-			// 		setStore({ message: data.message })
-			// 		// don't forget to return something, that is how the async resolves
-			// 		return data;
-			// 	}catch(error){
-			// 		console.log("Error loading message from backend", error)
-			// 	}
-			// },
-			// changeColor: (index, color) => {
-			// 	//get the store
-			// 	const store = getStore();
+			getMessage: async () => {
+				try{
+			 		// fetching data from the backend
+					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
+					const data = await resp.json()
+					setStore({ message: data.message })
+					// don't forget to return something, that is how the async resolves
+					return data;
+				}catch(error){
+					console.log("Error loading message from backend", error)
+				}
+			},
+			changeColor: (index, color) => {
+				//get the store
+				const store = getStore();
 
-			// 	//we have to loop the entire demo array to look for the respective index
-			// 	//and change its color
-			// 	const demo = store.demo.map((elm, i) => {
-			// 		if (i === index) elm.background = color;
-			// 		return elm;
-			// 	});
+				//we have to loop the entire demo array to look for the respective index
+				//and change its color
+				const demo = store.demo.map((elm, i) => {
+					if (i === index) elm.background = color;
+					return elm;
+				});
 
-			// 	//reset the global store
-			// 	setStore({ demo: demo });
-			// }
+				//reset the global store
+				setStore({ demo: demo });
+			}
 		}
 	};
 };
