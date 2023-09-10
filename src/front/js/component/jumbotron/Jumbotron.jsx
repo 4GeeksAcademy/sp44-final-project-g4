5a2473f60593f3b2215a9b20fa95389a98dcf60b
Yúliya Bundur <ybundur@gmail.com>
2023-09-10 20:55:57 +0000

    jumbotron in progres

--- src/front/js/component/jumbotron/Jumbotron.jsx
+++ src/front/js/component/jumbotron/Jumbotron.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 
 export const Jumbotron = () => {
     return (
-        <div class="jumbotron jumbotron-fluid bg-light-blue px-2 py-4">
-            <div class="container">
+        <div class="jumbotron jumbotron-fluid px-2 py-5">
+            <div class="container container-jumbotron py-3 px-4">
                 <h1 class="card-title-jumbotron"><b>All</b> the services <b>for your pet</b> in one place</h1>
+                <div class="spacer"></div>
                 <hr class="my-4"></hr>
                 <h4 class="card-text-jumbotron">Este es un mensaje importante que deseamos destacar.</h4>
-                    <a class="btn glow-on-hover btn-lg my-4" href="https://images.unsplash.com/photo-1535930749574-1399327ce78f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1936&q=80" role="button"><h5>Find professional</h5></a>
+                
+                <a class="btn custom-btn glow-on-hover btn-4 btn-lg my-2" href="https://images.unsplash.com/photo-1535930749574-1399327ce78f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1936&q=80" role="button"><h5>Find professional</h5></a>
             </div>
         </div>
     )
