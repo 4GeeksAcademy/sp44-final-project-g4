44e01ba46fd77cdfb296954cbd891748e18f46eb
El Hadj Mbengue Arroyo <109061270+arroyostack@users.noreply.github.com>
2023-09-17 18:10:19 +0000

    feat(frontend): Pop up for map markers

---
+++ src/front/js/helpers/getAllVets.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const getAllVets = async () => {
+    try {
+        const response = await fetch( "https://api.petfinder.com/v2/animals?type=dog&location=New York", requestOptions );
+        if ( !response.ok ) {
+            throw new Error( "Failed to fetch data, status code: " + response.status );
+        }
+        const data = await response.json();
+        return data;
+    } catch ( error ) {
+        console.error( "An error occurred while fetching data:", error );
+    } finally {
+        console.log( "Data fetch operation completed" );
+    }
+};
