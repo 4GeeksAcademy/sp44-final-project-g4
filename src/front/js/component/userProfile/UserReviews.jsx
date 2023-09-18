507c1a701baa420e06e31a85cdd5882b1fa93832
Hector Padron <helpad93@gmail.com>
2023-09-18 18:12:16 +0000

    Add: UserProfile.jsx

---
+++ src/front/js/component/userProfile/UserReviews.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+export const UserReviews = ()=> {
+    return (
+        <>
+            <ul>
+                <li>
+                    <div className="border border-2 mb-2">
+                        <div className="d-flex w-100 justify-content-between ">
+                            <h5 className="mb-1">Name of user</h5>
+                            <small>3 days ago</small>
+                        </div>
+                        <p className="mb-1">Aqui va el comentario del user.</p>
+                        <small>And some small print.</small>
+                    </div>
+                </li>
+            </ul>
+        </>
+    )
+}
