04e2333fa4cdea2a026c084fcfb7e9ba61c35914
El Hadj Mbengue Arroyo <109061270+arroyostack@users.noreply.github.com>
2023-09-22 09:16:26 +0000

    Flux backend url changed

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,5 +1,6 @@
+const backend = "https://sample-service-name-9dn1.onrender.com/"
 
-const getState = ( { getStore, getActions, setStore } ) => {
+const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			loggedUser: [],
@@ -8,73 +9,73 @@ const getState = ( { getStore, getActions, setStore } ) => {
 		},
 		actions: {
 
-			fetchData: async ( urlKey, storageKey, ) => {
+			fetchData: async (urlKey, storageKey,) => {
 				try {
 
-					if ( !localStorage.getItem( storageKey ) ) {
+					if (!localStorage.getItem(storageKey)) {
 
-						const host = "https://ideal-space-goggles-9r547pw47qq2p5vr-3001.app.github.dev"; // URL base
-						const url = `${ host }/api/professional/${ urlKey }`;
+						const host = backend; // URL base
+						const url = `${host}/api/professional/${urlKey}`;
 
 						const request = {
 							method: 'GET',
 							redirect: 'follow'
 						};
-						const response = await fetch( url, request );
-						if ( response.ok ) {
+						const response = await fetch(url, request);
+						if (response.ok) {
 							const data = await response.json();
-							setStore( { [ storageKey ]: data.results } );
-							localStorage.setItem( storageKey, JSON.stringify( data ) );
-							localStorage.removeItem( storageKey );
+							setStore({ [storageKey]: data.results });
+							localStorage.setItem(storageKey, JSON.stringify(data));
+							localStorage.removeItem(storageKey);
 							return data;
 						} else {
-							throw new Error( `Error: ${ response.status } - ${ response.statusText }` );
+							throw new Error(`Error: ${response.status} - ${response.statusText}`);
 						}
 
 
 					} else {
-						const data = JSON.parse( localStorage.getItem( storageKey ) );
+						const data = JSON.parse(localStorage.getItem(storageKey));
 						return data;
 					}
-				} catch ( error ) {
-					throw new Error( `Error fetching ${ urlKey }: ${ error.message }` );
+				} catch (error) {
+					throw new Error(`Error fetching ${urlKey}: ${error.message}`);
 				}
 			},
 
 
 			getAllProfessionals: async () => {
-				Promise.all( [
-					fetch( "https://miniature-trout-9rqg9vgq9jv2p959-3001.preview.app.github.dev/api/professional/vet" ),
-					fetch( "https://miniature-trout-9rqg9vgq9jv2p959-3001.preview.app.github.dev/api/professional/groomer" ),
-					fetch( "https://miniature-trout-9rqg9vgq9jv2p959-3001.preview.app.github.dev/api/professional/walker" )
-				] )
-					.then( responses => {
-						return Promise.all( responses.map( response => response.json() ) );
-					} )
-					.then( data => {
-						setStore( { vets: data[ 0 ].results } );
-						setStore( { vets: data[ 1 ].results } );
-						setStore( { vets: data[ 2 ].results } );
-						localStorage.setItem( "vets", JSON.stringify( data[ 0 ].results ) );
-						localStorage.setItem( "groomers", JSON.stringify( data[ 1 ].results ) );
-						localStorage.setItem( "walkers", JSON.stringify( data[ 2 ].results ) );
-						console.log( data[ 0 ] );
-						console.log( data[ 1 ] );
-						console.log( data[ 2 ] );
+				Promise.all([
+					fetch(`/vet`),
+					fetch(`/groomer`),
+					fetch(`/walker`)
+				])
+					.then(responses => {
+						return Promise.all(responses.map(response => response.json()));
+					})
+					.then(data => {
+						setStore({ vets: data[0].results });
+						setStore({ vets: data[1].results });
+						setStore({ vets: data[2].results });
+						localStorage.setItem("vets", JSON.stringify(data[0].results));
+						localStorage.setItem("groomers", JSON.stringify(data[1].results));
+						localStorage.setItem("walkers", JSON.stringify(data[2].results));
+						console.log(data[0]);
+						console.log(data[1]);
+						console.log(data[2]);
 
-					} );
+					});
 			},
 
 			logout: () => {
-				localStorage.removeItem( "avatar" );
-				localStorage.removeItem( "email" );
-				localStorage.removeItem( "id" );
-				localStorage.removeItem( "type" );
-				localStorage.removeItem( "user" );
-				setStore( { loggedUser: [] } );
+				localStorage.removeItem("avatar");
+				localStorage.removeItem("email");
+				localStorage.removeItem("id");
+				localStorage.removeItem("type");
+				localStorage.removeItem("user");
+				setStore({ loggedUser: [] });
 			},
 
-			addFavorite: ( user, professional ) => {
+			addFavorite: (user, professional) => {
 
 
 			}
