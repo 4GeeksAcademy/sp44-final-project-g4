7c01145410405a31593bc8461415e853798add9b
Romu9319 <129613538+Romu9319@users.noreply.github.com>
2023-10-05 10:25:19 +0000

    footer component modified

--- src/front/js/component/footer/footerComponents/DevelopersComponent.jsx
+++ src/front/js/component/footer/footerComponents/DevelopersComponent.jsx
@@ -1,29 +1,29 @@
 import React from 'react'
 
 
-export const DevelopersComponent = () => {
+export const DevelopersComponent = ({ image }) => {
     return (
-        <div className='col-md-4 col-lg-4 col-xl-4 mt-2 text-center'>
+        <div className='col-md-4 col-lg-4 col-xl-4 mt-1 text-center'>
             <h5 className='text-uppercase font-weight-bold text-center'>Developed By:</h5>
-            <hr className='mb-2' />
+            <hr className='mb-1' />
             <div className='text-center'>
                 <p>
-                    <img style={{ height: "35px", width: "35px", borderRadius: "50%" }} src="" alt="" className="p-1" />
+                    <img style={{ height: "35px", width: "35px", borderRadius: "50%" }} src={image} alt="" className="bg-dark" />
                     <a href="https://www.linkedin.com/in/yuliya-bundur/" className='text-dark mx-2'>Yuliya Bundur</a>
                     <a href="https://github.com/yuliyabundur"><i className="fab fa-github"></i></a>
                 </p>
                 <p>
-                    <img style={{ height: "35px", width: "35px", borderRadius: "50%" }} src="" alt="" className="p-1 bg-dark" />
+                    <img style={{ height: "35px", width: "35px", borderRadius: "50%" }} src={image} alt="" className="bg-dark" />
                     <a href="https://www.linkedin.com/in/elhadjarroyo/" className='text-dark mx-2'>El Hadj Mbaye Mbengue Arroyo</a>
                     <a href="https://github.com/tuusuario"><i className="fab fa-github"></i></a>
                 </p>
                 <p>
-                    <img style={{ height: "35px", width: "35px", borderRadius: "50%" }} src="" alt="" className="p-1 bg-dark" />
+                    <img style={{ height: "35px", width: "35px", borderRadius: "50%" }} src={image} alt="" className="bg-dark" />
                     <a href="https://www.linkedin.com/in/hector-padron-488288b0/" className='text-dark mx-2'>Hector Padron</a>
                     <a href="https://github.com/hectorpd"><i className="fab fa-github"></i></a>
                 </p>
                 <p>
-                    <img style={{ height: "35px", width: "35px", borderRadius: "50%" }} src="" alt="" className="p-1 bg-dark" />
+                    <img style={{ height: "35px", width: "35px", borderRadius: "50%" }} src={image} alt="" className="bg-dark" />
                     <a href="https://www.linkedin.com/in/rodr%C3%ADguez-mu%C3%B1oz/" className='text-dark mx-2'>Daniel E. Rodriguez Munoz</a>
                     <a href="https://github.com/Romu9319"><i className="fab fa-github"></i></a>
                 </p>
