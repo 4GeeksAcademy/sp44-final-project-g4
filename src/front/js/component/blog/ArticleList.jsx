cf81762b6bc460792036a1a28c536b17111cf7a4
Romu9319 <129613538+Romu9319@users.noreply.github.com>
2023-10-05 20:03:03 +0000

    footer and blog articles updated

--- src/front/js/component/blog/ArticleList.jsx
+++ src/front/js/component/blog/ArticleList.jsx
@@ -6,33 +6,31 @@ import { Link } from "react-router-dom";
 
 export const ArticleList = () => {
 
-    const { store, actions } = useContext( Context );
-    const [ articles, setArticles ] = useState( JSON.parse( localStorage.getItem( "posts" ) ) );
+    const { store, actions } = useContext(Context);
+    const [articles, setArticles] = useState(JSON.parse(localStorage.getItem("posts")));
 
 
     return (
-        <div className="d-flex justify-content-center m-5">
-            <div className="row">
-                { articles && articles.results.map( ( article ) => (
-                    <div className='col-md-4 col-lg-4 col-xl-4 mt-4 mb-4' key={ article.id }>
-                        <div className='card'>
-                            <img className='card-img-bottom' src={ article.image } />
-                            <div className="card-body">
-                                <h3 className='card-title'>{ article.title }</h3>
-                                <p className="fst-italic">by: { article.author }</p>
-                            </div>
-                            <div className="d-flex justify-content-between align-items-center m-2">
-                                <span className="card-text text-start m-2">{ article.category }</span>
-                                <Link to={ `/blog/${ article.id }` } className="btn shadow-sm m-2">Read...</Link>
-                            </div>
 
-                        </div>
-
+        <div className="row d-flex justify-content-center">
+            {articles && articles.results.map((article) => (
+                <div className='card col-md-3 col-lg-3 col-xl-3 m-2 px-0' key={article.id} style={{ maxWidth: "20rem", maxHeight: "30rem" }}>
+                    <div className='card-img-bottom'>
+                        <img src={article.image} style={{ width: "100%", height: "12rem" }} />
                     </div>
-                ) )
-                }
-            </div>
-        </div>
+                    <div className="card-body">
+                        <h5 className='title'>{article.title}</h5>
+                        <p className="fst-italic">by: {article.author}</p>
+                        <span className="fst-italic">Category: {article.category}</span>
+                    </div>
+                    <div className="d-flex justify-content-end m-2">
+                        <Link to={`/blog/${article.id}`} className="btn shadow-sm m-2">Read...</Link>
+                    </div>
+                </div>
+            ))
+            }
+        </div >
+
 
     );
 };
--- src/front/js/component/footer/Footer.jsx
+++ src/front/js/component/footer/Footer.jsx
@@ -9,15 +9,15 @@ const dummyPicture = "https://free-url-shortener.rb.gy/url-shortener.png";
 export const Footer = () => {
     return (
 
-        <footer className='footer mt-auto bg-light text-dark p-1 project-light'>
+        <footer className='footer bg-light text-dark mt-5 p-1 project-light'>
             <div className='container text-center text-md-start'>
                 <div className='row text-center text-md-start'>
-                    {/* Aqui poner el componente con el logo de la web ... pedirle a alayi el logo*/ }
-                    <LogoComponent image={ "https://img.freepik.com/free-vector/cute-dog-cat-friend-cartoon_138676-2432.jpg?w=2000" } />
+                    {/* Aqui poner el componente con el logo de la web ... pedirle a alayi el logo*/}
+                    <LogoComponent image={"https://img.freepik.com/free-vector/cute-dog-cat-friend-cartoon_138676-2432.jpg?w=2000"} />
                     <InfographicComponent />
-                    <DevelopersComponent />        
+                    <DevelopersComponent />
                 </div>
             </div>
         </footer>
     );
-    }
+}
