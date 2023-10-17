b645e40e44d02ca603917949c75efeec82dca3b8
El Hadj Mbengue Arroyo <109061270+arroyostack@users.noreply.github.com>
2023-10-17 16:38:39 +0000

    Fixed: unused component error in deploy.

--- src/front/js/component/SiteTitle.jsx
+++ src/front/js/component/SiteTitle.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import mainLogo from '/workspaces/sp44-final-project-g4/src/front/img/logo-web.png';
-import { Link } from 'react-router-dom';
 
 
 export const SiteTitle = () => {
     return (
         <div className="logo-web">
-            <Link to="/">
-                <img src={mainLogo} alt="" style={{ width: "100px", height: "80px", marginTop: "15px", marginBottom: "15px", marginLeft: "10px" }} />
-            </Link>
+
         </div>
     );
 };
