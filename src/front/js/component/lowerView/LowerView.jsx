74d57dc28d867713b044c411eaa12e62261257a6
Yúliya Bundur <ybundur@gmail.com>
2023-10-18 22:08:06 +0000

    changes in lowerview

--- src/front/js/component/lowerView/LowerView.jsx
+++ src/front/js/component/lowerView/LowerView.jsx
@@ -2,19 +2,23 @@ import React from "react";
 import { CardVet } from "./cards/CardVet.jsx";
 import { CardGroomer } from "./cards/CardGroomer.jsx";
 import { CardWalker } from "./cards/CardWalker.jsx";
+
 import { CardAdoption } from "./cards/CardAdoption.jsx";
 
+
+
+
 export const LowerView = () => {
-  return (
-    <>
-      <div className="container">
-        <div className="d-flex justify-content-between">
+    return (
+       
+        <div className="container text-center mb-5 ps-0 pe-0" style={{ width: "80rem" }}>
+        <div className="d-flex flex-wrap">
           <div className="flex-fill me-3"><CardVet /></div>
           <div className="flex-fill me-3"><CardGroomer /></div>
           <div className="flex-fill me-3"><CardWalker /></div>
           <div className="flex-fill"><CardAdoption /></div>
         </div>
       </div>
-    </>
-  );
-};
+       
+    );
+}; 
--- src/front/js/component/lowerView/cards/CardWalker.jsx
+++ src/front/js/component/lowerView/cards/CardWalker.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const CardWalker = () => {
-
     return (
         <>
             <div className="card mb-3 main-light-blue-bg">
                 <div className="card-header">Walkers</div>
                 <Link to={`/views/`} state="walker">
                     <img
-                        src="https://images.pexels.com/photos/7210537/pexels-photo-7210537.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
-                        className="w-50"
+                        src="https://images.pexels.com/photos/7210537/pexels-photo-7210537.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        style={{ width: "240px", height: "160px" }}  // Establece el tamaño aquí
                         alt="Walkers"
                     />
                 </Link>
